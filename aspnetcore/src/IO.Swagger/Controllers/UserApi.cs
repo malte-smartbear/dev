0eac9484dc0caa9bc99b0eb076ec8ce98b615aa1
editorone <editortestone@gmail.com>
2019-05-17 11:21:13 +0000

    Generated code by SwaggerHub

--- aspnetcore/src/IO.Swagger/Controllers/UserApi.cs
+++ aspnetcore/src/IO.Swagger/Controllers/UserApi.cs
@@ -112,41 +112,6 @@ namespace IO.Swagger.Controllers
         }
 
         /// <summary>
-        /// Get user by user name
-        /// </summary>
-        
-        /// <param name="username">The name that needs to be fetched. Use user1 for testing.</param>
-        /// <response code="200">successful operation</response>
-        /// <response code="400">Invalid username supplied</response>
-        /// <response code="404">User not found</response>
-        [HttpGet]
-        [Route("/malt-v5-team/all-plugins/1.0.0/user/{username}")]
-        [ValidateModelState]
-        [SwaggerOperation("GetUserByName")]
-        [SwaggerResponse(statusCode: 200, type: typeof(User), description: "successful operation")]
-        public virtual IActionResult GetUserByName([FromRoute][Required]string username)
-        { 
-            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(200, default(User));
-
-            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(400);
-
-            //TODO: Uncomment the next line to return response 404 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
-            // return StatusCode(404);
-
-            string exampleJson = null;
-            exampleJson = "{\n  \"firstName\" : \"firstName\",\n  \"lastName\" : \"lastName\",\n  \"password\" : \"password\",\n  \"userStatus\" : 6,\n  \"phone\" : \"phone\",\n  \"id\" : 0,\n  \"email\" : \"email\",\n  \"username\" : \"username\"\n}";
-            exampleJson = "<User>\n  <id>123456789</id>\n  <username>aeiou</username>\n  <firstName>aeiou</firstName>\n  <lastName>aeiou</lastName>\n  <email>aeiou</email>\n  <password>aeiou</password>\n  <phone>aeiou</phone>\n  <userStatus>123</userStatus>\n</User>";
-            
-            var example = exampleJson != null
-            ? JsonConvert.DeserializeObject<User>(exampleJson)
-            : default(User);
-            //TODO: Change the data returned
-            return new ObjectResult(example);
-        }
-
-        /// <summary>
         /// Logs user into the system
         /// </summary>
         
