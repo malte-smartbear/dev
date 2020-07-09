f80def5c6998d3c865ea275e1efa2c13948775b0
SwaggerHub <SwaggerHub@smartbear.com>
2020-07-09 11:01:45 +0000

    API definition transferred by SwaggerHub

---
+++ inflector/src/main/java/io/swagger/controllers/PetController.java
@@ -0,0 +1,37 @@
+package io.swagger.controllers;
+
+import io.swagger.oas.inflector.models.RequestContext;
+import io.swagger.oas.inflector.models.ResponseContext;
+import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import java.io.File;
+import java.util.List;
+
+import io.swagger.model.*;
+
+import io.swagger.model.ModelApiResponse;
+import io.swagger.model.Pet;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaInflectorServerCodegen", date = "2020-07-09T11:01:45.420Z[GMT]")public class PetController  {
+  /** 
+   * Uncomment and implement as you see fit.  These operations will map
+   * Directly to operation calls from the routing logic.  Because the inflector
+   * Code allows you to implement logic incrementally, they are disabled.
+   **/
+
+  /*
+    public ResponseContext addPet(RequestContext request , Pet body 
+) {
+        return new ResponseContext().status(Status.INTERNAL_SERVER_ERROR).entity( "Not implemented" );
+    }
+  */
+
+  /*
+    public ResponseContext updatePet(RequestContext request ) {
+        return new ResponseContext().status(Status.INTERNAL_SERVER_ERROR).entity( "Not implemented" );
+    }
+  */
+
+}
+
